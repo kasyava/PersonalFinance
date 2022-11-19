7adc5ca33a1be18d1cfc38bbd3d6984fae81939b
kasyava <budo_slava@mail.ru>
2022-11-19 21:28:15 +0600

    add auth route

--- server/app.js
+++ server/app.js
@@ -6,11 +6,15 @@ const helmet = require('helmet');
 
 const config = require('./config');
 
+const auth = require('./routes/Auth');
+
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api/auth', auth);
 
 
 const dbConnect = () => {
