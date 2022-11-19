e7fe515631aadd17ac4848f43ee88a5ca8ae4575
kasyava <budo_slava@mail.ru>
2022-11-19 21:22:49 +0600

    create simple mongoose scheme for User

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bCrypt = require('bcryptjs');
+// const nanoid = require('nanoid');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    username: {type: String},
+    password: {
+        type: String,
+        require: true
+    }
+}, {timestamps: true});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bCrypt.genSaltSync(10);
+    this.password = await bCrypt.hashSync(this.password, salt);
+
+    next();
+});
+
+UserSchema.methods.checkPassword = function (password) {
+    return bCrypt.compareSync(password, this.password);
+};
+
+UserSchema.index({email: 1});
+
+const Users = mongoose.model('Users', UserSchema);
+
+module.exports = Users;
