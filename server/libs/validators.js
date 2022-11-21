0e749ecefd30f61d1d4b1726bb9e9f4206346e2c
kasyava <budo_slava@mail.ru>
2022-11-21 20:59:49 +0600

    Добавил валидатор ObjectId

---
+++ server/libs/validators.js
@@ -0,0 +1,12 @@
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+// MongoDB ObjectId validator
+exports.isValidObjectId = (id) => {
+
+    if (ObjectId.isValid(id)) {
+        return (String)(new ObjectId(id)) === id;
+
+    }
+    return false;
+}
