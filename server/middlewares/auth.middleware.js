e15974c708d7e3c19d086e86c73a20484833bbd5
kasyava <budo_slava@mail.ru>
2022-11-21 20:50:22 +0600

    Auth middleware created

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const memcached = require('../libs/memcached/memcached');
+const {isValidObjectId} = require('../libs/validators')
+
+const config = require('../config');
+const User = require('../models/User.model');
+
+const auth = (req, res, next) => {
+    const header = req.headers['authorization'];
+    if (!header) return res.status(401).json({message: "Unauthorized"});
+    const token = header.split(' ')[1];
+    if (!token) return res.status(401).json({message: "Unauthorized"});
+
+    jwt.verify(token, config.JWT_SECRET, async (err, decoded) => {
+
+        if (err) return res.status(401).json({message: "Unauthorized"});
+        if (!isValidObjectId(decoded.userId)) return res.status(400).json({message: "Bad request"});
+        let user;
+
+        user = await memcached.get(`${req.hostname}-user-${decoded.userId}`);
+
+        if (!user) user = await User.findOne({_id: decoded.userId})
+        if (!user || (new Date(user.tokenLastDate).getTime() !== new Date(decoded.iat * 1000).getTime())) return res.status(401).json({message: "Unauthorized"});
+
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = auth;
