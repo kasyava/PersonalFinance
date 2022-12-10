998cdc68ef836a42e2eee7d642762bdc1c8fcdd5
kasyava <budo_slava@mail.ru>
2022-12-10 13:51:44 +0600

    fixtures update

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const config = require('./config');
 
-const User = require('./models/User');
+const User = require('./app/models/userModel');
+const Category = require('./app/models/categoryModel');
 
 mongoose.connect(config.DB.URL + '/' + config.DB.NAME);
 
@@ -13,18 +14,38 @@ db.once('open', async () => {
 
     try {
         await db.dropCollection('users');
+        console.log('USERS is dropped');
     } catch (e) {
-        console.log(e.message)
+
         console.log('Collections USERS where not present, skipping drop...');
     }
-    console.log('USERS is dropped');
 
-    await User.create({
+
+    try {
+        await db.dropCollection('categories');
+        console.log('CATEGORIES is dropped');
+    } catch (e) {
+
+        console.log('Collections CATEGORIES where not present, skipping drop...');
+    }
+
+
+    const user = await User.create({
         username: '123456789',
         password: '123456789',
         email: '123456789@gmail.com'
     });
     console.log('USERS created');
+
+    await Category.create({
+        title: 'Bred',
+        user: user._id,
+    }, {
+        title: "Bear",
+        user: user._id
+    });
+    console.log('CATEGORIES created');
+
 
     console.log('All collections were created');
     db.close();
